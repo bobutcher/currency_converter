86c42b32aa2a9431006c5a437d35da3e54ccd8b7
Bo Butcher <cbbutcher91@gmail.com>
2016-03-10 18:00:17 -0500

    just getting started

---
+++ currency.rb
@@ -0,0 +1,22 @@
+class Currency
+  def initialize(string)
+    split_money = string.split("")
+    currency_type = split_money.shift
+    @numerics - split_money.join("").to_f
+    @currency_code = symbol_map[@symbol]
+    symbol_map = {"$" => :USD, "€" => :EUR, "¥" => :JPY}
+  end
+
+  def amount
+    @amount
+  end
+
+  def currency_code
+    @currency_code = currency_code
+  end
+
+  def symbol
+    @symbol
+  end
+  def to_s
+end
---
+++ currency_converter.rb
@@ -0,0 +1,6 @@
+class Currency
+  def initialize(symbol, currency, rates)
+    @symbol = symbol
+    @currency = currency
+    @rates = rates
+  end
