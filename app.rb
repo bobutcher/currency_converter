5d902359155e1f2887d23997f40b1ea3bc5b3d40
Bo Butcher <cbbutcher91@gmail.com>
2016-04-12 20:30:21 -0400

    More clean up

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 require_relative 'currency'            # => true
 require_relative 'currency_converter'  # => true
 
-  c1 = Currency.new("$10.00")   # => #<Currency:0x007fbd0a9f00f0 @symbol="$", @numerics=10.0, @currency_type=:USD>
-  c2 = Currency.new("$10.00")   # => #<Currency:0x007fbd0a9ebca8 @symbol="$", @numerics=10.0, @currency_type=:USD>
-  c3 = Currency.new("€10.00")   # => #<Currency:0x007fbd0a9eb370 @symbol="€", @numerics=10.0, @currency_type=:EUR>
-  c4 = Currency.new("€4.00")    # => #<Currency:0x007fbd0a9ea9e8 @symbol="€", @numerics=4.0, @currency_type=:EUR>
-  c5 = Currency.new("¥100.00")  # => #<Currency:0x007fbd0a9ea420 @symbol="¥", @numerics=100.0, @currency_type=:JPY>
-  c6 = Currency.new("¥200.00")  # => #<Currency:0x007fbd0a9e9cc8 @symbol="¥", @numerics=200.0, @currency_type=:JPY>
+  c1 = Currency.new("$10.00")   # => #<Currency:0x007ff9fd8826c0 @symbol="$", @numerics=10.0, @currency_type=:USD>
+  c2 = Currency.new("$10.00")   # => #<Currency:0x007ff9fd881ef0 @symbol="$", @numerics=10.0, @currency_type=:USD>
+  c3 = Currency.new("€10.00")   # => #<Currency:0x007ff9fd881928 @symbol="€", @numerics=10.0, @currency_type=:EUR>
+  c4 = Currency.new("€4.00")    # => #<Currency:0x007ff9fd881388 @symbol="€", @numerics=4.0, @currency_type=:EUR>
+  c5 = Currency.new("¥100.00")  # => #<Currency:0x007ff9fd87b438 @symbol="¥", @numerics=100.0, @currency_type=:JPY>
+  c6 = Currency.new("¥200.00")  # => #<Currency:0x007ff9fd87af38 @symbol="¥", @numerics=200.0, @currency_type=:JPY>
 
   #can be multiplied by a float
-  c2 * 0.25         # => #<Currency:0x007fbd0a9e9750 @symbol="$", @numerics=2.5, @currency_type=:USD>
+  c2 * 0.25         # => #<Currency:0x007ff9fd87a920 @symbol="$", @numerics=2.5, @currency_type=:USD>
   #can be multiplied by a fixnum
-  c3 * 2            # => #<Currency:0x007fbd0a9e91b0 @symbol="€", @numerics=20.0, @currency_type=:EUR>
+  c3 * 2            # => #<Currency:0x007ff9fd87a448 @symbol="€", @numerics=20.0, @currency_type=:EUR>
   #shows currency code
   c6.currency_type  # => :JPY
 
@@ -28,14 +28,14 @@ require_relative 'currency_converter'  # => true
   #can be subtracted with the same currency code
   c2 - c1  # => 0.0
 
-    currency_test = CurrencyConverter.new({:USD => 1, :EUR => 1.13919, :JPY => 0.00922})  # => #<CurrencyConverter:0x007fbd0a9e2a40 @rate={:USD=>1, :EUR=>1.13919, :JPY=>0.00922}>
+  currency_test = CurrencyConverter.new({:USD => 1, :EUR => 1.13919, :JPY => 0.00922})  # => #<CurrencyConverter:0x007ff9fd8793e0 @rate={:USD=>1, :EUR=>1.13919, :JPY=>0.00922}>
 
-  c7 = currency_test.convert(c1, :JPY)  # => #<Currency:0x007fbd0a9e25e0 @symbol="1", @numerics=84.5986984815618, @currency_type=nil>
+  c7 = currency_test.convert(c1, :JPY)  # => #<Currency:0x007ff9fd878cb0 @symbol="1", @numerics=84.5986984815618, @currency_type=nil>
 
   c8 = currency_test.convert(c1, :AUD)  # ~> UnknownCurrencyCodeError: I don't know that code
 
-  # ~> UnknownCurrencyCodeError
-  # ~> I don't know that code
-  # ~>
-  # ~> /Users/bobutcher/Documents/currency_converter/currency_converter.rb:16:in `convert'
-  # ~> /Users/bobutcher/Documents/currency_converter/app.rb:35:in `<main>'
+# ~> UnknownCurrencyCodeError
+# ~> I don't know that code
+# ~>
+# ~> /Users/bobutcher/Documents/currency_converter/currency_converter.rb:15:in `convert'
+# ~> /Users/bobutcher/Documents/currency_converter/app.rb:35:in `<main>'
--- currency.rb
+++ currency.rb
@@ -1,53 +1,42 @@
 class Currency
-  # attr_reader :currency_type, :numerics                   # => nil
+  attr_reader :currency_type, :numerics, :symbol
   def initialize(string)
-    string
     split_money = string.chars
     @symbol = split_money.shift
-    symbol_map = {"$" => :USD, "€" => :EUR, "¥" => :JPY}
+    symbol_map = { '$' => :USD, '€' => :EUR, '¥' => :JPY }
     @numerics = split_money.join.to_f
     @currency_type = symbol_map[@symbol]
   end
+
   def +(other)
     if @symbol == other.symbol
-    other.numerics + self.numerics
-  else
-    raise DifferentCurrencyCodeError, "Cannot add different currencies together"
+      other.numerics + numerics
+    else
+      raise DifferentCurrencyCodeError, 'Cannot add different currencies together'
     end
   end
+
   def -(other)
     if @symbol == other.symbol
-     other.numerics - self.numerics
-   else
-    raise DifferentCurrencyCodeError, "Cannot subtract different currencies together"
+      other.numerics - numerics
+    else
+      raise DifferentCurrencyCodeError, 'Cannot subtract different currencies together'
     end
   end
 
   def ==(other)
     if @symbol == other.symbol
-    other.numerics == self.numerics
-  else
-    false
+      other.numerics == numerics
+    else
+      false
     end
-  end
-
-  def currency_type
-    return @currency_type
-  end
-
-  def numerics
-     @numerics
-  end
-
-  def symbol
-     @symbol
   end
 
   def *(other)
     multi_var = @numerics * other.to_f
-     Currency.new("#{@symbol}#{multi_var}")
+    Currency.new("#{@symbol}#{multi_var}")
   end
 end
 
-  class DifferentCurrencyCodeError < StandardError
-  end
+class DifferentCurrencyCodeError < StandardError
+end
